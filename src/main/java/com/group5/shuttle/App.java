d2435e47efac5cc34fd6e226e58a043d7b3486ae
Bassma Toubbali <bassma.toubbali@sap.com>
2026-03-27 16:20:35 +0100

    Initial project setup with JavaFX and project structure

---
+++ src/main/java/com/group5/shuttle/App.java
@@ -0,0 +1,13 @@
+package com.group5.shuttle;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Shuttle Dashboard");
+        stage.show();
+    }
+}
---
+++ src/main/java/com/group5/shuttle/controller/InventoryController.java
@@ -0,0 +1,5 @@
+package com.group5.shuttle.controller;
+
+public class InventoryController {
+    
+}
